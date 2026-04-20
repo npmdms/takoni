c9503010117312ea1bd43bf83b7c1c5ba502331e
dimasgusti <dimas_gusti@icloud.com>
2026-04-20 22:28:19 +0800

    feat: add script for external widget in TestPage2

--- app/test/test-2/page.tsx
+++ app/test/test-2/page.tsx
@@ -2,6 +2,10 @@ export default function TestPage2() {
   return (
     <>
       <div>
+        <script
+          src="https://takoni-osjmjfc3l-gustidimas-projects.vercel.app/resmeku/resbot/widget.js"
+          async
+        ></script>
         <h1>Test 2</h1>
       </div>
     </>

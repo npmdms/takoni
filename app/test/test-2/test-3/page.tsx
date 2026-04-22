8238b0779555137f595a691c0118b7d21b7f1a93
dimasgusti <dimas_gusti@icloud.com>
2026-04-22 22:12:50 +0800

    feat: add TestPage2 component with external widget script

---
+++ app/test/test-2/test-3/page.tsx
@@ -0,0 +1,13 @@
+export default function TestPage2() {
+  return (
+    <>
+      <div>
+        <script
+          src="https://takoni.vercel.app//resmeku/resbot/widget.js"
+          async
+        ></script>
+        <h1>Test 2</h1>
+      </div>
+    </>
+  );
+}
